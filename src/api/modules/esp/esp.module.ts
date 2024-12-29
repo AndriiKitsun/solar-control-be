d3b68500199a83b02ea7cde1015caa7eda3a0bc3
Andrii Kitsun <andrii.kitsun@gmail.com>
2024-12-29 16:47:55 +0200

    update (#18)

--- src/api/modules/esp/esp.module.ts
+++ src/api/modules/esp/esp.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EspApiService } from './esp.service';
 import { ESP_WS_SERVICE } from './esp.constants';
-import { FakeEspWsService } from './ws/fake-esp-ws.service';
 import { HttpClientModule } from '../../common';
+import { EspWsService } from './ws/esp-ws.service';
 
 @Module({
   imports: [HttpClientModule],
@@ -10,7 +10,7 @@ import { HttpClientModule } from '../../common';
     EspApiService,
     {
       provide: ESP_WS_SERVICE,
-      useClass: FakeEspWsService,
+      useClass: EspWsService,
     },
   ],
   exports: [EspApiService, ESP_WS_SERVICE],
