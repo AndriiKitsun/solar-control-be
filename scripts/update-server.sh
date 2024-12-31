31f1fd506d3de18c41e6d85b891579be4b9a30c1
Andrii Kitsun <Andrii.Kitsun@gmail.com>
2024-12-31 20:02:38 +0200

    Fetch tags in update script

--- scripts/update-server.sh
+++ scripts/update-server.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
 
-PROJECT_PATH="/solar-control-be"
+PROJECT_PATH="."
 
 cd $PROJECT_PATH || exit
 echo "Current directory: $(pwd)"
 
-echo "Pulling a new server version..."
-git pull origin dev
+echo "Fetching new tags from remote..."
+git fetch --tags
+
+latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+
+echo "Checking out latest tag: $latestTag"
+git checkout "${latestTag}"
 
 echo "Installing project dependencies..."
 npm ci
@@ -15,4 +20,3 @@ echo "Building the application..."
 npm run build
 
 echo "Done"
-exit 0
