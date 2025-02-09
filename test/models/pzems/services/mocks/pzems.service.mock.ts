ff2ef22d436e02c67c804d00015b78c6f64678b6
Andrii Kitsun <andrii.kitsun@gmail.com>
2025-02-09 14:56:37 +0200

    SC-00: Async pzem table clearing on startup (#36)
    * SC-00: Async pzem table clearing
    
    * add changeset

--- test/models/pzems/services/mocks/pzems.service.mock.ts
+++ test/models/pzems/services/mocks/pzems.service.mock.ts
@@ -5,7 +5,7 @@ import { ClassMock } from '@common/types/test.types';
 import { EspApiServiceMock } from '@api/modules/esp/mocks/esp-service.mock';
 
 export class PzemsServiceMock implements ClassMock<PzemsService> {
-  async onModuleInit(): Promise<void> {
+  onModuleInit(): void {
     return;
   }
 
